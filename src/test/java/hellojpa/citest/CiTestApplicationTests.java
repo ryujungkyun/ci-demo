0953e97e790f69a6c8371eb8338f05398b10bfe9
RyuJungKyun <prank01@naver.com>
2026-08-13 11:09:53 +0900

    ci: add CI pipeline with OIDC

---
+++ src/test/java/hellojpa/citest/CiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package hellojpa.citest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
